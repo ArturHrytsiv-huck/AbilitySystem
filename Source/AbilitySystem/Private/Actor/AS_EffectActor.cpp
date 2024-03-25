694d4606350f22c47e568894ca3611a24e9bfaf2
ArturHrytsiv-huck <arturgritsiv3@gmail.com>
2024-03-25 13:56:04 +0200

    gameplay effects v1

--- Source/AbilitySystem/Private/Actor/AS_EffectActor.cpp
+++ Source/AbilitySystem/Private/Actor/AS_EffectActor.cpp
@@ -2,44 +2,28 @@
 
 
 #include "Actor/AS_EffectActor.h"
-
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
+
 #include "AbilitySystemInterface.h"
-#include "AbilitySystem/AS_AttributeSet.h"
 
 
 AAS_EffectActor::AAS_EffectActor()
 {
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-	
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
-void AAS_EffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-	const FHitResult& SweepResult)
+void AAS_EffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	// TODO: FIX
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UAS_AttributeSet* AS_AttributeSet = Cast<UAS_AttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAS_AttributeSet::StaticClass()));
-
-		UAS_AttributeSet* MutableAttributeSet = const_cast<UAS_AttributeSet*>(AS_AttributeSet);
-		MutableAttributeSet->SetHealth(AS_AttributeSet->GetHealth() + 25.f);
-	}
-}
-
-void AAS_EffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (TargetASC == nullptr) return;
 	
-}
-
-void AAS_EffectActor::BeginPlay()
-{
-	Super::BeginPlay();
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
 	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAS_EffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAS_EffectActor::OnEndOverlap);
+	FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
--- Source/AbilitySystem/Public/Actor/AS_EffectActor.h
+++ Source/AbilitySystem/Public/Actor/AS_EffectActor.h
@@ -7,6 +7,8 @@
 #include "GameFramework/Actor.h"
 #include "AS_EffectActor.generated.h"
 
+class UGameplayEffect;
+
 UCLASS()
 class ABILITYSYSTEM_API AAS_EffectActor : public AActor
 {
@@ -15,19 +17,14 @@ class ABILITYSYSTEM_API AAS_EffectActor : public AActor
 public:
 	AAS_EffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION()
-	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-	
 protected:
-	virtual void BeginPlay() override;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 	
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffect;
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 };
