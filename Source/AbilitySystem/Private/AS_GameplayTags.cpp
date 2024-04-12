93e0149d1c2360401928267490d382e8900d7621
ArturHrytsiv-huck <arturgritsiv3@gmail.com>
2024-04-12 11:00:27 +0300

    fix input tags

--- Source/AbilitySystem/Private/AS_GameplayTags.cpp
+++ Source/AbilitySystem/Private/AS_GameplayTags.cpp
@@ -58,16 +58,16 @@ void FAS_GameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag
 		(FName("InputTag.LMB"), FString("Input tag for Left Mouse Button"));	
 
-	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag
 		(FName("InputTag.1"), FString("Input tag for 1 Key"));
 
-	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag
 		(FName("InputTag.2"), FString("Input tag for 2 Key"));
 		
-	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag
 		(FName("InputTag.3"), FString("Input tag for 3 Key"));
 		
-	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag
 		(FName("InputTag.4"), FString("Input tag for 4 Key"));
 		
 }
